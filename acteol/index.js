074c00bc2abcb3462882199a5c172341b84a2002
Lionel Martin <hi@getlionel.com>
2020-05-09 11:56:38 +0100

    creates a contact, needs refreshing the session before each request, otherwise the access_token seems short lived

---
+++ acteol/index.js
@@ -0,0 +1,57 @@
+const contactCreate = require('./creates/contact');
+// const basketCreate = require('./creates/basket');
+
+const authentication = require('./authentication');
+
+// To include the Authorization header on all outbound requests, simply define a function here.
+// It runs runs before each request is sent out, allowing you to make tweaks to the request in a centralized spot
+const includeBearerToken = (request, z, bundle) => {
+  if (bundle.authData.access_token) {
+    request.headers['Authorization'] = `Bearer ${bundle.authData.access_token}`
+  }
+  return request;
+};
+
+const sessionRefreshIf401 = (response, z, bundle) => {
+  if (bundle.authData.access_token) {
+    if (response.status === 401) {
+      throw new z.errors.RefreshAuthError(); // ask for a refresh & retry
+    }
+  }
+  return response;
+};
+
+// We can roll up all our behaviors in an App.
+const App = {
+  // This is just shorthand to reference the installed dependencies you have. Zapier will
+  // need to know these before we can upload
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+  authentication: authentication,
+
+  // beforeRequest & afterResponse are optional hooks into the provided HTTP client
+  beforeRequest: [
+    includeBearerToken
+  ],
+
+  afterResponse: [
+    sessionRefreshIf401
+  ],
+
+  // If you want to define optional resources to simplify creation of triggers, searches, creates - do that here!
+  resources: {},
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: {},
+
+  // If you want your searches to show up, you better include it here!
+  searches: {},
+
+  // If you want your creates to show up, you better include it here!
+  creates: {
+    [contactCreate.key]: contactCreate,
+  }
+};
+
+// Finally, export the app.
+module.exports = App;
