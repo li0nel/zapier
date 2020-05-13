ac1e5faece300f5523ff5f33dc1e2a1e537b533e
Lionel Martin <hi@getlionel.com>
2020-05-13 20:37:51 +0100

    success!!

--- airship/creates/contact.js
+++ airship/creates/contact.js
@@ -3,20 +3,20 @@ const contactSearch = require('../searches/contact').operation.perform;
 
 const createContactIfNotExists = (z, bundle) => {
   return contactSearch(z, bundle).then(contact => {
-    return contact.empty ? createContact(z, bundle).then(c => c) : contact[0];
+    return contact.length ? contact[0] : createContact(z, bundle).then(c => c);
   })
 }
 
 const createContact = (z, bundle) => {
   // let xml = fs.readFileSync(path.resolve(__dirname, '../soap-envelopes/create_contact.xml'), 'utf-8');
-  let xml = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://secure.airship.co.uk/SOAP/V3/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><SOAP-ENV:Body><ns1:createContact><username xsi:type="xsd:string">{{username}}</username><password xsi:type="xsd:string">{{password}}</password><contactData xsi:type="ns1:contactObject"><firstname xsi:type="xsd:string">{{first_name}}</firstname><mobilenumber xsi:type="xsd:string">{{mobile_number}}</mobilenumber><email xsi:type="xsd:string">{{email}}</email><allowemail xsi:type="xsd:string">{{allow_email}}</allowemail><allowsms xsi:type="xsd:string">{{allow_email}}</allowsms><sourceid xsi:type="xsd:string">{{source_id}}</sourceid></contactData><groups SOAP-ENC:arrayType="xsd:int[1]" xsi:type="ns1:GroupArray"><item xsi:type="xsd:int">{{group_id}}</item></groups><consents SOAP-ENC:arrayType="ns1:updateContactConsent[1]" xsi:type="ns1:updateContactConsentArray"><item xsi:type="ns1:updateContactConsent"><consenttypeid xsi:type="xsd:string">1</consenttypeid><consentstatus xsi:type="xsd:string">{{allow_email}}</consentstatus></item></consents></ns1:createContact></SOAP-ENV:Body></SOAP-ENV:Envelope>'
+  let xml = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://secure.airship.co.uk/SOAP/V3/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><SOAP-ENV:Body><ns1:createContact><username xsi:type="xsd:string">{{username}}</username><password xsi:type="xsd:string">{{password}}</password><contactData xsi:type="ns1:contactObject"><firstname xsi:type="xsd:string">{{first_name}}</firstname><email xsi:type="xsd:string">{{email}}</email><allowemail xsi:type="xsd:string">{{allow_email}}</allowemail><allowsms xsi:type="xsd:string">{{allow_email}}</allowsms><sourceid xsi:type="xsd:string">{{source_id}}</sourceid></contactData><groups SOAP-ENC:arrayType="xsd:int[1]" xsi:type="ns1:GroupArray"><item xsi:type="xsd:int">{{group_id}}</item></groups><consents SOAP-ENC:arrayType="ns1:updateContactConsent[1]" xsi:type="ns1:updateContactConsentArray"><item xsi:type="ns1:updateContactConsent"><consenttypeid xsi:type="xsd:string">1</consenttypeid><consentstatus xsi:type="xsd:string">{{allow_email}}</consentstatus></item></consents></ns1:createContact></SOAP-ENV:Body></SOAP-ENV:Envelope>'; // <mobilenumber xsi:type="xsd:string">{{mobile_number}}</mobilenumber>
     
   xml = xml.replace(/{{username}}/g, bundle.authData.soap_username);
   xml = xml.replace(/{{password}}/g, bundle.authData.soap_password);
   xml = xml.replace(/{{first_name}}/g, bundle.inputData.first_name);
-  xml = xml.replace(/{{mobile_number}}/g, bundle.inputData.mobile_number);
+  // xml = xml.replace(/{{mobile_number}}/g, bundle.inputData.mobile_number);
   xml = xml.replace(/{{email}}/g, bundle.inputData.email);
-  xml = xml.replace(/{{allow_email}}/g, bundle.inputData.allow_email);
+  xml = xml.replace(/{{allow_email}}/g, 'Y'); //TODO
   xml = xml.replace(/{{source_id}}/g, bundle.inputData.source_id);
   xml = xml.replace(/{{group_id}}/g, bundle.inputData.group_id);
 
