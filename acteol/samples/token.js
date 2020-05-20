1e3a049b8431fd92f4a456a3fe56dac83fa570f3
Lionel Martin <hi@getlionel.com>
2020-05-20 11:44:36 +0100

    removing real email addresses from samples...

--- acteol/samples/token.js
+++ acteol/samples/token.js
@@ -2,7 +2,7 @@ module.exports = {
     "access_token": "xxxxxx",
     "token_type": "bearer",
     "expires_in": 86399,
-    "userName": "lionel@wi5.io",
+    "userName": "kanye@wi5.io",
     ".issued": "Wed, 29 Apr 2020 14:50:54 GMT",
     ".expires": "Thu, 30 Apr 2020 14:50:54 GMT"
 }
